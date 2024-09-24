e83c6ac762a0a4877a9005a3c142edbf14ea1b40
Ayub-Ian <iayub.mia@gmail.com>
2024-09-24 16:28:40 +0300

    auction create and get endpoint

--- AuctionService/app/controllers/AuctionController.py
+++ AuctionService/app/controllers/AuctionController.py
@@ -1,8 +1,37 @@
+import inflection
+from flask import request
 from flask_restful import Resource
 
-from app.models import Auction
+from app.models import Auction, Item
+from app import db
 
 class AuctionResource(Resource):
+    def get(self, auction_id):
+        auction = db.session.get(Auction, {"id": auction_id})
+
+        if not auction:
+            return {"message": "No auction found with id."}, 404
+
+        return {
+                "id": auction.id,
+                "createdAt": auction.created_at.isoformat(),
+                "updatedAt": auction.updated_at.isoformat(),
+                "auctionEnd": auction.auction_end.isoformat(),
+                "seller": auction.seller,
+                "winner": auction.winner,
+                "make": auction.item.make,
+                "model": auction.item.model,
+                "year": auction.item.year,
+                "color": auction.item.color,
+                "mileage": auction.item.mileage,
+                "imageUrl": auction.item.image_url,
+                "status": auction.status.name,
+                "reservePrice": auction.reserve_price,
+                "soldAmount": auction.sold_amount,
+                "currentHighBid": auction.current_high_bid
+            }, 200 
+
+class AuctionsResource(Resource):
     def get(self):
         auctions = Auction.query.all()
         return [
@@ -25,3 +54,38 @@ class AuctionResource(Resource):
                 "currentHighBid": auction.current_high_bid
             } for auction in auctions
         ], 200
+
+    def post(self):
+        seller = "test"
+        data = request.get_json()
+
+        entities = {inflection.underscore(k):v for k,v in data.items()}
+
+        auction = Auction(
+            reserve_price = entities["reserve_price"],
+            auction_end = entities["auction_end"],
+            seller = seller,
+            item = Item (
+                image_url = entities["image_url"],
+                mileage = entities["mileage"],
+                model = entities["model"],
+                make = entities["make"],
+                color = entities["color"],
+                year = entities["year"]
+            )
+        )
+
+        try:
+            db.session.add(auction)
+            db.session.commit()
+            return {"message": f"successfully created auction {auction.id}"}
+         
+        except Exception as e:
+            db.session.rollback()
+            return {"message": str(e)}, 500
+
+
+
+
+
+        return {"data": entities}, 201
--- AuctionService/app/routes/auction.py
+++ AuctionService/app/routes/auction.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from app.controllers.AuctionController import AuctionResource
+from app.controllers.AuctionController import AuctionResource, AuctionsResource
 
 routes = Blueprint('auctions', __name__)
 
 api = Api(routes)
 
-api.add_resource(AuctionResource, '/auctions')
+api.add_resource(AuctionsResource, '/auctions')
+api.add_resource(AuctionResource, '/auctions/<string:auction_id>')
